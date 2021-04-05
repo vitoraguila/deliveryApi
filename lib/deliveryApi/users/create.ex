d633ef0b7fecfc5fe2ffa8fad3b69c6b98a8a31a
Vitor Aguila <vitor.aguila1@gmail.com>
2021-04-05 21:27:42 +0200

    flexibility from error status

--- lib/deliveryApi/users/create.ex
+++ lib/deliveryApi/users/create.ex
@@ -5,5 +5,12 @@ defmodule DeliveryApi.Users.Create do
     params
     |> User.changeset()
     |> Repo.insert()
+    |> handle_insert()
+  end
+
+  defp handle_insert({:ok, %User{}} = result), do: result
+
+  defp handle_insert({:error, result}) do
+    {:error, %{status: :reques, result: result}}
   end
 end
